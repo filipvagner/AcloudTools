98da627eea1a210bafb1444f7f2287cd3b3cfd51
Filip Vagner <filip.vagner@heidelbergcement.com>
2021-11-26 13:59:34 +0100

    Removed duplicated file

---
+++ GetMyTestCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcloudTools
+{
+    [Cmdlet(VerbsCommon.Get, "MyTestCommand")]
+    [OutputType(typeof(string))]
+    public class GetMyTestCommand : Cmdlet
+    {
+        protected override void ProcessRecord()
+        {
+            PowerShell psCommand = PowerShell.Create();
+            Collection<PSObject> psCommandOutput = new Collection<PSObject>();
+
+            psCommand.AddCommand("Get-AzAccessToken");
+            PSObject accessToken = (from tokenItem in psCommand.Invoke()
+                                    where tokenItem.Members["Token"] != null
+                                    select tokenItem).First();
+
+            WriteObject(accessToken.Members["Token"].Value, true);
+        }
+    }
+}
